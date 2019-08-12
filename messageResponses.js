48cf5b31829b55a6f560f09ea71f6746f724f5f5
Nicola-Marie O'Riordan <nicriordan@gmail.com>
2019-08-12 16:25:38 +0100

    Create messageResponses.js

---
+++ messageResponses.js
@@ -0,0 +1,26 @@
+module.exports = {
+        "invocationMessage" : {
+									type: "SSML",
+									ssml: '<speak><voice name="Matthew">This is your invocation message.</voice></speak>'
+								},
+        "unknownMessage"     : {
+									type: "PlainText",
+									text: "I'm unsure what to do now"
+								},
+ 
+	  "firstIntentMessage"  : {
+									type: "SSML",
+									ssml: '<speak><voice name="Matthew">This is your first intent</voice></speak>'
+								},
+								
+	  "secondIntentMessage" : {
+									type: "SSML",
+									ssml: '<speak><voice name="Matthew">This is your second intent</voice></speak>'
+								},
+
+	  "thirdIntentMessage"  : {
+									type: "SSML",
+									ssml: '<speak><voice name="Matthew">This is your third intent</voice></speak>'
+								}
+                           
+}
